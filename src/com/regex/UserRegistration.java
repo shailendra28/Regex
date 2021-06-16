37b7d15ea4b009afa5f79d95c1aa84ecb9109572
shailendra28 <yshailendrakumar1768@gmail.com>
2021-06-17 01:46:38 +0530

    Checking Last Name as per the Pattern

--- src/com/regex/UserRegistration.java
+++ src/com/regex/UserRegistration.java
@@ -7,24 +7,37 @@ import java.util.regex.Pattern;
  */
 public class UserRegistration {
     private boolean check;
-    private String firstName;
+    private String firstName,lastName;
     Scanner sc = new Scanner(System.in);
     public void checkFirstName() {
         System.out.println("enter first name starts with capital with minimum Three character : ");
         firstName = sc.nextLine();
         //check whether the enter first name matches the pattern
         check = Pattern.compile("[A-Z]{1}[a-z]{2,}").matcher(firstName).matches();
-        if(!check){
+        if (!check) {
             System.out.println(" first name is invalid ");
             checkFirstName();
-        }
-        else {
+        } else {
             System.out.println(" first name is valid ");
         }
     }
+        public void checkLastName() {
+            System.out.println("enter Last name starts with capital with minimum Three character : ");
+            lastName = sc.nextLine();
+            //check whether the enter Last name matches the pattern
+            check = Pattern.compile("[A-Z]{1}[a-z]{2,}").matcher(lastName).matches();
+            if (!check) {
+                System.out.println(" last name is invalid ");
+                checkLastName();
+            }
+            else
+                System.out.println(" last name is valid ");
+        }
+
     //Declare Main Class
     public static void main(String args[]) {
         UserRegistration user = new UserRegistration();
         user.checkFirstName();//Calling checkFirstName
+        user.checkLastName();//Calling checkLastName
     }
 }
