0be2c835a3ee6cd5ce6edd796285e2934c76bf9b
shailendra28 <yshailendrakumar1768@gmail.com>
2021-06-17 03:57:24 +0530

    Added Junit in the program

---
+++ src/main/java/com/regex/ValidateUser.java
@@ -0,0 +1,99 @@
+package com.regex;
+
+import java.util.regex.Pattern;
+/**
+ * Program for UserRegistration
+ * Test Case For firstName,lastName, email,phoneNumber,password(PASSWORDRULE1,PASSWORDRULE2,PASSWORDRULE3,PASSWORDRULE4
+ */
+public class ValidateUser {
+    private static final String FIRST_NAME_PATTERN = "^[A-Z]{1}[a-z]{3,}$";
+    private static final String LAST_NAME_PATTERN = "^[A-Z]{1}[a-z]{3,}$";
+    private static final String EMAIL_PATTERN= "[0-9 a-z A-z]+([._+-][0-9 a-z A-Z]+)*"+"@([0-9 a-z A-Z][-]?)+[.][a-z A-Z]{2,4}([.][a-z A-Z]{2,4})?$";
+    private static final String MOB_NUMBER = "^[1-9]{2}\\s{0,1}[0-9]{5}[0-9]{5}$";
+    private static final String PASSWORDRULE1 = "[a-z]{8}";
+    private static final String PASSWORDRULE2 =  "[a-z A-Z]{8}";
+    private static final String PASSWORDRULE3 = "[0-9 a-z A-Z]{8}";
+    private static final String PASSWORDRULE4 = "^(?=.?[A-Z])(?=(.[a-z]){1,})(?=[.]{1,})(?=1,})(?!.*\\s).{8,}$";
+
+    public static boolean validateFirstName(String fname) {
+        Pattern pattern = Pattern.compile(FIRST_NAME_PATTERN);
+        return pattern.matcher(fname).matches();
+
+    }
+    public static boolean validateLastName(String lname) {
+        Pattern pattern = Pattern.compile(LAST_NAME_PATTERN);
+        return pattern.matcher(lname).matches();
+
+    }
+
+    public static boolean validateEmail(String email) {
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        return pattern.matcher(email).matches();
+    }
+
+    public static boolean validateMobNumber(String mobnumber) {
+        Pattern pattern = Pattern.compile(MOB_NUMBER);
+        return pattern.matcher(mobnumber).matches();
+    }
+
+    public static boolean validatePasswordRule1(String password1) {
+        Pattern pattern = Pattern.compile(PASSWORDRULE1);
+        return pattern.matcher(password1).matches();
+    }
+
+    public static boolean validatePasswordrule2(String password2) {
+        Pattern pattern = Pattern.compile(PASSWORDRULE2);
+        return pattern.matcher(password2).matches();
+    }
+
+    public static boolean validatePasswordRule3(String password3) {
+        Pattern pattern = Pattern.compile(PASSWORDRULE3);
+        return pattern.matcher(password3).matches();
+    }
+
+    public static boolean validatePasswordRule4(String password4) {
+        Pattern pattern = Pattern.compile(PASSWORDRULE4);
+        return pattern.matcher(password4).matches();
+    }
+
+    public static String validatefirstname(String fname) {
+        String firstnamepattern = "^[A-Z]{1}[a-z]{3,}$";
+        if (Pattern.matches(firstnamepattern, fname))
+            return "Happy";
+        else
+            return "Sad";
+    }
+
+    public static String validatelastname(String lname) {
+        String lastnamepattern = "^[A-Z]{1}[a-z]{3,}$";
+        if (Pattern.matches(lastnamepattern, lname))
+            return "Happy";
+        else
+            return "Sad";
+    }
+
+    public static String validateemail(String email) {
+        String emailnamepattern = "[0-9 a-z A-z]+([._+-][0-9 a-z A-Z]+)*"+"@([0-9 a-z A-Z][-]?)+[.][a-z A-Z]{2,4}([.][a-z A-Z]{2,4})?$";
+        if (Pattern.matches(emailnamepattern, email))
+            return "Happy";
+        else
+            return "Sad";
+    }
+
+    public static String validatelMobNumber(String mobnum) {
+        String mobnumpattern = "^[1-9]{2}\\s{0,1}[0-9]{5}[0-9]{5}$";
+        if (Pattern.matches(mobnumpattern, mobnum))
+            return "Happy";
+        else
+            return "Sad";
+
+    }
+
+    public static String validatPassword(String password) {
+        String Passwordpattern = "[0-9 a-z A-Z]{8}";
+        if (Pattern.matches(Passwordpattern, password))
+            return "Happy";
+        else
+            return "Sad";
+    }
+}
---
+++ src/test/java/com/regex/ValidateUserTest.java
@@ -0,0 +1,150 @@
+package com.regex;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+/**
+ * Program for UserRegistration
+ * Test Case For firstName,lastName, email,phoneNumber,password(uppercase,NumericNumber,special char) Using regex concept
+ */
+public class ValidateUserTest {
+    //Test case for FirstName and return True
+    @Test
+    void givenFirstName_WhenProper_ShouldReturnTrue() {
+        ValidateUser validator = new ValidateUser();
+        boolean result = ValidateUser.validateFirstName("Shailendra");
+        Assertions.assertEquals(true, result);
+    }
+    //Test case for FirstName and return False
+    @Test
+    public void givenFirstName_WhenShort_ShouldReturnFalse() {
+        ValidateUser validator = new ValidateUser();
+        boolean result = ValidateUser.validateFirstName("Sh");
+        Assertions.assertEquals(false, result);
+    }
+    //Test case for LastName and return True
+    @Test
+    public void givenLastName_WhenProper_ShouldReturnTrue() {
+        ValidateUser validator = new ValidateUser();
+        boolean result = ValidateUser.validateLastName("Yadav");
+        Assertions.assertEquals(true,result);
+    }
+    //Test case for LastName and return false
+    @Test
+    public void givenLastName_WhenShort_ShouldReturnFalse() {
+        ValidateUser validator = new ValidateUser();
+        boolean result = ValidateUser.validateLastName("Sha");
+        Assertions.assertEquals(false,result);
+    }
+    //Test case for email and return True
+    @Test
+    public void givenEmail_WhenProper_ShouldReturnTrue() {
+        ValidateUser validator = new ValidateUser();
+        boolean result = ValidateUser.validateEmail("abc.xyz@bl.co.in");
+        Assertions.assertEquals(true,result);
+    }
+    //Test case for MobNumber and return true
+    @Test
+    public void givenMobNumber_WhenProper_ShouldReturnTrue() {
+        ValidateUser validator = new ValidateUser();
+        boolean result = ValidateUser.validateMobNumber("91 8779796556");
+        Assertions.assertEquals(true,result);
+    }
+    //Test case for MobNumber and return False
+    @Test
+    public void givenMobNumber_WhenDifferent_ShouldReturnFalse() {
+        ValidateUser validator = new ValidateUser();
+        boolean result = ValidateUser.validateMobNumber("91 87mj88B886");
+        Assertions.assertEquals(false, result);
+    }
+    //Test case for PasswordRule1 and return True
+    @Test
+    public void givenPasswordRule1_WhenProper_ShouldReturnTrue() {
+        ValidateUser validator = new ValidateUser();
+        boolean result = ValidateUser.validatePasswordRule1("mkijhgtr");
+        Assertions.assertEquals(true,result);
+    }
+    //Test case for PasswordRule2 and return True
+    @Test
+    public void givenPasswordRule2_WhenProper_ShouldReturnTrue(){
+        ValidateUser validator = new ValidateUser();
+        boolean result = ValidateUser.validatePasswordrule2("Ghutkytf");
+        Assertions.assertEquals(true,result);
+    }
+    //Test case for PasswordRule3 and return True
+    @Test
+    public void givenPasswordRule3_WhenProper_ShouldReturnTrue() {
+        ValidateUser validator = new ValidateUser();
+        boolean result = ValidateUser.validatePasswordRule3("jdF5iOj8");
+        Assertions.assertEquals(true, result);
+    }
+    //Test case for PasswordRule4 and return False
+    @Test
+    public void givenPasswordRule4_WhenProper_ShouldReturnFalse() {
+        ValidateUser validator = new ValidateUser();
+        boolean result = ValidateUser.validatePasswordRule4("Eyu6@uyh");
+        Assertions.assertEquals(false,result);
+    }
+    //Test case for firstname and return Happy
+    @Test
+    public void givenFirstName_WhenProper_ShouldReturnHappy() {
+        ValidateUser validator = new ValidateUser();
+        String result = ValidateUser.validatefirstname("Shailendra");
+        Assertions.assertEquals("Happy", result);
+    }
+    //Test case for firstname and return Sad
+    @Test
+    public void givenFirstName_WhenShort_ShouldReturnSad() {
+        ValidateUser validator = new ValidateUser();
+        String result = ValidateUser.validatefirstname("Sh");
+        Assertions.assertEquals("Sad", result);
+    }
+    //Test case for lastname and return Happy
+    @Test
+    public void givenLastName_WhenProper_ShouldReturnHappy() {
+        ValidateUser validator = new ValidateUser();
+        String result = ValidateUser.validatelastname("Yadav");
+        Assertions.assertEquals("Happy", result);
+    }
+    //Test case for lastname and return Sad
+    @Test
+    public void givenLastName_WhenShort_ShouldReturnSad() {
+        ValidateUser validator = new ValidateUser();
+        String result = ValidateUser.validatelastname("Yad");
+        Assertions.assertEquals("Sad", result);
+    }
+    //Test case for email return Happy
+    @Test
+    public void givenEmail_WhenProper_ShouldReturnHappy() {
+        ValidateUser validator = new ValidateUser();
+        String result = ValidateUser.validateemail("abc.xyz@bl.co.in");
+        Assertions.assertEquals("Happy", result);
+    }
+    //Test case for email return Sad
+    @Test
+    public void givenEmail_WhenNotProper_ShouldReturnSad() {
+        ValidateUser validator = new ValidateUser();
+        String result = ValidateUser.validateemail("ascffrg");
+        Assertions.assertEquals("Sad", result);
+    }
+    //Test case for MobNumber return Sad
+    @Test
+    public void givenMobNumber_WhenNotProper_ShouldReturnSad() {
+        ValidateUser validator = new ValidateUser();
+        String result = ValidateUser.validatelMobNumber("9969550645");
+        Assertions.assertEquals("Sad", result);
+    }
+    //Test case for Password return Happy
+    @Test
+    public void givenPassword_WhenProper_ShouldReturnHappy() {
+        ValidateUser validator = new ValidateUser();
+        String result = ValidateUser.validatPassword("A6rtr59k");
+        Assertions.assertEquals("Happy", result);
+    }
+    //Test case for Password return Sad
+    @Test
+    public void givenPassword_WhenNotProper_ShouldReturnSad() {
+        ValidateUser validator = new ValidateUser();
+        String result = ValidateUser.validatPassword("A6@tr59k");
+        Assertions.assertEquals("Sad", result);
+    }
+}
